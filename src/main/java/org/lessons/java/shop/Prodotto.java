bb307f2eed2e41bcad71197262009074084c0714
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-06-12 11:59:50 +0200

    add: added class Prodotto

---
+++ src/main/java/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,41 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+
+	// VARIABILI D'ISTANZA
+	private int code;
+	private String name;
+	private String description;
+	private double price, iva;
+	
+	// COSTRUTTORE
+	public Prodotto(String name, String description, double price, double iva) {
+		this.code = (int) Math.random() * 1000;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.iva = iva;
+	}
+	
+	
+	// METODO CHE RESTITUISCE IL PREZZO BASE
+	public double getPrice() {
+		return this.price;
+	}
+	
+	// METODO CHE RESTITUISCE IL PREZZO COMPRENSIVO D'IVA
+	public double getPriceIva() {
+		return this.price + this.iva;
+	}
+	
+	// METODO CHE RESTITUISCE IL NOME-CODICE
+	public String getNameCode() {
+		return this.code + "-" + this.name;
+	}
+	
+	// METODO CHE RESTITUISCE LA DESCRIZIONE
+	public String getDescription() {
+		return this.description;
+	}
+	
+}
